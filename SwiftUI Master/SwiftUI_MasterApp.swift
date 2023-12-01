bb3c4aa8c6300eee97d402fcd6ae4b9e1a2db772
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-12-01 21:36:59 +0300

    Initial Commit

---
+++ SwiftUI Master/SwiftUI_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MasterApp.swift
+//  SwiftUI Master
+//
+//  Created by Furkan Cingöz on 1.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
